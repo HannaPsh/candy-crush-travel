19d27fc78b33cd2eb03a1b905af8543236a896b0
Hanna Pshanichnaya <hannapshanich@gmail.com>
2022-09-02 01:59:43 +0200

    pages added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Gameapp from './components/Gameapp';
+import ScoreBoard from './components/ScoreBoard';
+import ChooseProfilePicturePage from './components/ChooseProfilePicturePage';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      {/* <h1>Application</h1> */}
+      <Router>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Router>
+      <Login />
+      <Gameapp/>
+      <ScoreBoard/>
+      <ChooseProfilePicturePage />
     </div>
   );
 }
