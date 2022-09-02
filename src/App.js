d23199869a539b33eb0500c297588ebf032df8ef
Hanna Pshanichnaya <hannapshanich@gmail.com>
2022-09-02 02:09:39 +0200

    packages added

--- src/App.js
+++ src/App.js
@@ -4,22 +4,15 @@ import Login from './components/Login';
 import Gameapp from './components/Gameapp';
 import ScoreBoard from './components/ScoreBoard';
 import ChooseProfilePicturePage from './components/ChooseProfilePicturePage';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="wrapper">
-      {/* <h1>Application</h1> */}
-      <Router>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Switch>
-      </Router>
+      <Dashboard />
       <Login />
-      <Gameapp/>
-      <ScoreBoard/>
+      <Gameapp />
+      <ScoreBoard />
       <ChooseProfilePicturePage />
     </div>
   );
